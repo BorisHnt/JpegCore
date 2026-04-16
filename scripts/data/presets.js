700b57d3cedc756e7d64c11a08be16b0d6025baa
Boris Hanicotte <bhanicot@c1r18s3.marseille.42.school>
2026-04-16 17:06:03 +0200

    random commit

--- scripts/data/presets.js
+++ scripts/data/presets.js
@@ -9,7 +9,38 @@ export const starterTexts = [
   "do not steal",
   "preview only",
   "princesse du 13",
-  "emo boy 2007"
+  "emo boy 2007",
+  "jtm mon bb",
+  "lach t coms",
+  "msn 4ever",
+  "kissouxxx",
+  "je gere tro",
+  "t en mode bogoss",
+  "jtaime en force",
+  "ptdr xd",
+  "sisi la mif",
+  "en couple tkt",
+  "fashion victim",
+  "miss du 94",
+  "bg officiel",
+  "mon coeur en mousse",
+  "trop fresh",
+  "xoxo glam",
+  "bff pour la vie",
+  "repond sur msn",
+  "tof prise o tel",
+  "100% vrai love",
+  "secret story vibe",
+  "attention fille fragile",
+  "posey trankil",
+  "soit pas jalou",
+  "star de mon sky",
+  "peace love tuning",
+  "made in 2007",
+  "trop belle ou rien",
+  "juste moi et mes delires",
+  "bbey d'amouur",
+  "comm'z rendus"
 ];
 
 export const watermarkTextPresets = [
@@ -32,7 +63,26 @@ export const worsenWords = [
   "xoxo",
   "swag",
   "princesse",
-  "vip"
+  "vip",
+  "kissou",
+  "mdr",
+  "ptdr",
+  "bogoss",
+  "belle gosse",
+  "fashion",
+  "glam",
+  "bb",
+  "jaloux",
+  "msn",
+  "coms",
+  "bff",
+  "tmtc",
+  "sisi",
+  "fresh",
+  "luv",
+  "baby",
+  "angel",
+  "star"
 ];
 
 export const catastrophePresets = [
@@ -47,5 +97,29 @@ export const catastrophePresets = [
     backgroundId: "bg-lightning",
     frameId: "frame-chrome",
     text: "bg de la night"
+  },
+  {
+    label: "MSN Romance",
+    backgroundId: "bg-angel-cloud",
+    frameId: "frame-hearts-glitter",
+    text: "jtm mon bb"
+  },
+  {
+    label: "Skyblog Queen",
+    backgroundId: "bg-disco-roses",
+    frameId: "frame-pearl-princess",
+    text: "lach t coms"
+  },
+  {
+    label: "Kikoo Drama",
+    backgroundId: "bg-emo-checker",
+    frameId: "frame-emo-black-pink",
+    text: "comm'z rendus"
+  },
+  {
+    label: "Love Toxic 2007",
+    backgroundId: "bg-pink-acid",
+    frameId: "frame-pink-gems",
+    text: "en couple tkt"
   }
 ];
